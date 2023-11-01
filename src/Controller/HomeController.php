48b5a00baeba282953b61a85975b2031b2e1b1f4
Katoru <katoru-sama@hotmail.fr>
2023-11-01 22:35:38 +0100

    Creation de la webapp, premier controller fonctionnel, premier template fonctionnel, style ok, localisation fonctionnelle

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Translation\LocaleSwitcher;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+
+  public function __construct(private TranslatorInterface $translator, private LocaleSwitcher $localeSwitcher)
+  {
+  }
+
+  #[Route('/', name: 'home')]
+  public function home(Request $request): Response
+  {
+    $this->localeSwitcher->setLocale('en');
+    $this->localeSwitcher->setLocale('fr');
+    $this->localeSwitcher->setLocale('fi');
+    return $this->render('index/index.html.twig', []);
+  }
+}
