4df4c08422d5de38a2cf8fe4f681092b8f2fa0f8
Katoru <katoru-sama@hotmail.fr>
2023-11-02 01:20:15 +0100

    Working localisation selector, locale is stored in 'locale' cookie for 30 days

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\Translation\LocaleSwitcher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,18 +13,42 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
 
-  public function __construct(private TranslatorInterface $translator, private LocaleSwitcher $localeSwitcher)
+  private $locales_values;
+  private $locales_labels;
+
+  public function __construct(private TranslatorInterface $translator, private LocaleSwitcher $localeSwitcher, string $locales_values, string $locales_labels)
   {
+    $this->locales_values = $locales_values;
+    $this->locales_labels = $locales_labels;
   }
 
   #[Route('/', name: 'home')]
   public function home(Request $request): Response
   {
-    $this->localeSwitcher->setLocale('en');
-    $this->localeSwitcher->setLocale('fr');
-    // $this->localeSwitcher->setLocale('fi');
-    return $this->render('index/index.html.twig', [
+    $newLocale = $request->cookies->get('locale');
+    if ($request->query->get('lang') != null)
+      $newLocale = $request->query->get('lang');
+    if ($request->request->get('lang') != null)
+      $newLocale = $request->request->get('lang');
+
+    $locales_values = explode("|", $this->locales_values);
+    $locales_labels = explode("|", $this->locales_labels);
+    if (in_array($newLocale, $locales_values)) {
+      $this->localeSwitcher->setLocale($newLocale);
+    }
+
+    $expires = time() + (86400 * 30);
+    $cookie = Cookie::create('locale', $newLocale,  $expires);
+    //$cookie = $response->headers->setCookie(Cookie::create('foo', 'bar'));
+    $response = new Response();
+    $response->headers->setCookie($cookie);
+    $content = $this->renderView('index/index.html.twig', [
       'locale' => $this->localeSwitcher->getLocale(),
+      'locales_values' => $locales_values,
+      'locales_labels' => $locales_labels,
     ]);
+    $response->setContent($content);
+
+    return $response;
   }
 }
